d65f603b18918dc959db4346e99674004aec9de4
karenhaining <79682066+karenhaining@users.noreply.github.com>
2024-08-12 14:45:09 -0400

    Integration tests for camp-generated files (#1)
    ---------
    
    Co-authored-by: Brian Sipos <brian.sipos@jhuapl.edu>

---
+++ tests/integration_test/test_sql_integration.py
@@ -0,0 +1,59 @@
+import psycopg2
+import os
+import ace
+import pytest
+
+from .util import ADMS_DIR, adm_files, run_camp
+
+
+@pytest.fixture(scope="session", autouse=True)
+def setup():
+    """
+    Connects to the ADMS library session. Cleans up connections once done.
+    @pre: IP Address of the library session should be stored in env var $PGHOST,
+          username and password should be stored in env vars $PGSQL_USERNAME and
+          $PGSQL_PASSWORD, respectively
+    @yields tuple of (connection object, AdmSet())
+    """
+
+    # setup: connect to ANMS library
+    conn = psycopg2.connect(
+            host=os.environ["PGHOST"],
+            port=int(os.environ.get("PGPORT", 5432)),
+            user=os.environ["PGUSER"],
+            password=os.environ["PGPASSWORD"]
+    )
+    cursor = conn.cursor()
+    
+    # reusable objects that the tests will need
+    yield cursor,
+
+    # teardown: close connections
+    cursor.close()
+    conn.close()
+
+
+@pytest.mark.parametrize("adm", adm_files())
+def test_adms(setup, adm):
+    """
+    Integration test for an ADM found in the ADMS_DIR folder
+    Resulting sql files will be placed in ADMS_DIR/amp-sql/Agent_Scripts and executed in the anms library.
+    """
+    cursor = setup[0]
+
+    if adm == 'ion_bpsec_admin.json':
+        pytest.xfail("ADM with known issue")
+    # input file full filepath
+    filepath = os.path.join(ADMS_DIR, adm)
+
+    # run camp
+    exitcode = run_camp(filepath, ADMS_DIR, only_sql=True, only_ch=False)
+    assert 0 == exitcode
+
+    # execute sql
+    adm_set = ace.AdmSet()
+    norm_name = adm_set.load_from_file(filepath).norm_name
+    sql_file = os.path.join(ADMS_DIR, "amp-sql", "Agent_Scripts", 'adm_{name}.sql'.format(name=norm_name))
+    with open(sql_file, "r") as f:
+        cursor.execute(f.read())
+        cursor.execute("rollback")
