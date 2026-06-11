f84808319f2617e3b81b46d64365604d47fb99ce
Brian Sipos <brian.sipos@jhuapl.edu>
2026-06-11 19:45:11 -0400

    Update for CACE mutex API and reduce some logging (#82)
    * Update for CACE mutex API and reduce some logging
    
    * update unit tests

--- test/data/gen_ch/example_test.c
+++ test/data/gen_ch/example_test.c
@@ -15,6 +15,7 @@
 #include <cace/amm/semtype.h>
 #include <cace/ari/text.h>
 #include <cace/util/logging.h>
+#include <cace/util/mutex.h>
 #include <cace/util/defs.h>
 
 /*   START CUSTOM INCLUDES HERE */
@@ -297,7 +298,7 @@ int refda_adm_example_test_init(refda_agent_t *agent)
 {
     CHKERR1(agent);
     CACE_LOG_DEBUG("Registering ADM: " "example-test");
-    REFDA_AGENT_LOCK(agent, REFDA_AGENT_ERR_LOCK_FAILED);
+    CACE_MUTEX_LOCK(&agent->objs_mutex);
 
     /*   START CUSTOM PRE-INIT HERE */
     /*   STOP CUSTOM PRE-INIT HERE  */
@@ -952,7 +953,7 @@ int refda_adm_example_test_init(refda_agent_t *agent)
     /*   START CUSTOM POST-INIT HERE */
     /*   STOP CUSTOM POST-INIT HERE  */
 
-    REFDA_AGENT_UNLOCK(agent, REFDA_AGENT_ERR_LOCK_FAILED);
+    CACE_MUTEX_UNLOCK(&agent->objs_mutex);
     return 0;
 }
 /*  STOP GENERATED SOURCE HERE */
