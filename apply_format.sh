bb70530a89221c1bac5ddfc32d2a3577b531db10
Brian Sipos <brian.sipos@jhuapl.edu>
2026-07-27 11:42:36 -0400

    Use ruff for format and lint

--- apply_format.sh
+++ apply_format.sh
@@ -31,4 +31,4 @@ SELFDIR=$(realpath $(dirname "${BASH_SOURCE[0]}"))
 cd ${SELFDIR}
 
 # Python source and test fixtures
-autopep8 -ir src test integration-test
+ruff format src test docs integration-test
