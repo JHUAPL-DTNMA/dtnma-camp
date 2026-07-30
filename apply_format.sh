6a5dd46d7ff55d8d0d6db0b05a3ea6abcdfb1a21
Brian Sipos <brian.sipos@jhuapl.edu>
2026-07-30 13:13:15 -0400

    Consolidate source formatting

--- apply_format.sh
+++ apply_format.sh
@@ -31,4 +31,6 @@ SELFDIR=$(realpath $(dirname "${BASH_SOURCE[0]}"))
 cd ${SELFDIR}
 
 # Python source and test fixtures
-ruff format src test docs integration-test
+SRC_DIRS="src test docs integration-test"
+ruff format ${SRC_DIRS}
+ruff check --fix ${SRC_DIRS}
